6ce3ee30c9c01255296cd54ff41af47830229eb9
Yinhui <zengyinhui@msn.com>
2015-06-13 22:50:23 +0800

    testing with py

---
+++ PythonTest.py
@@ -0,0 +1,14 @@
+"""Dialectizer for Python
+
+This program is part of "Dive Into Python", a free Python book for
+experienced programmers.  Visit http://diveintopython.org/ for the
+latest version.
+"""
+
+__author__ = "Evan (evan@qq.com)"
+__version__ = "$Revision: 1.2 $"
+__date__ = "$Date: 2004/05/05 21:57:19 $"
+__copyright__ = "Copyright (c) 2001 Mark Pilgrim"
+__license__ = "Python"
+
+print(__version__,__author__);
